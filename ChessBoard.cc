92088d8724eb2e71e1cac78b19b1483e625568f3
Kevin Gao <kevinmingjiang@gmail.com>
2022-07-21 15:46:11 -0400

    chessboard definition

--- ChessBoard.cc
+++ ChessBoard.cc
@@ -1,1 +1,28 @@
 #include "ChessBoard.h"
+using namespace std;
+
+ChessBoard::ChessBoard() {
+    // Construct 8 x 8 chessboard
+    theBoard = vector<vector<Piece *>>(8, vector<Piece *>(8, nullptr));
+}
+ChessBoard::~ChessBoard() {
+    for (int r = 0; r < theBoard.size(); ++r) {
+        for (int c = 0; c < theBoard[0].size(); ++c) {
+            if (theBoard[r][c]) {
+                delete theBoard[r][c];
+            }
+        }
+    }
+}
+
+void ChessBoard::defaultInit() {}
+
+vector<vector<Piece *>> ChessBoard::getBoard() {}
+
+vector<vector<Piece *>> getBoard() {}
+vector<Piece *> getWhiteRooks() {}
+vector<Piece *> getBlackRooks() {}
+vector<Piece *> getWhiteKing() {}
+vector<Piece *> getBlackKing() {}
+vector<Piece *> getWhitePieces() {}
+vector<Piece *> getBlackPieces() {}
--- ChessBoard.h
+++ ChessBoard.h
@@ -9,11 +9,14 @@ class Piece;
 
 class ChessBoard {
   private:
-    std::vector<std::vector<Piece *>> theBoard;
+    std::vector<std::vector<Piece *>> theBoard;  // store 8 x 8 chessboard
 
   public:
+    ChessBoard();
+    ~ChessBoard();
+    void defaultInit();  // Initialize standard 8 x 8 board pieces
     std::vector<std::vector<Piece *>> getBoard();
-    std::vector<Piece *> getWhiteRooks();  // first rook should be the one to the left
+    std::vector<Piece *> getWhiteRooks();
     std::vector<Piece *> getBlackRooks();
     std::vector<Piece *> getWhiteKing();
     std::vector<Piece *> getBlackKing();
