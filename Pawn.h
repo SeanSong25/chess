e3e219daa7b13833e0b947a9b4baa4c3c7e886da
Ke Xu <kexu1324@gmail.com>
2022-07-23 11:48:59 -0400

    update en passant implementation

--- Pawn.h
+++ Pawn.h
@@ -4,19 +4,13 @@
 
 class Pawn : public Piece{
     private:
-        bool enPassant;
-        Piece *enPassantPiece;
-        Position enPassantPosition;
         void updatePossibleNextPos() override;
     public:
         Pawn(std::shared_ptr<ChessBoard>, Colour, Position);
         Pawn(Pawn &);
-        bool isEnPassant();
-        void setEnpassant(Piece *) override;
         Piece *getEnPassantPiece();
         void checkEnPassant(Position) override;
-        Pawn *clone() override;
-        void afterMove() override;
+        Pawn *clone() override; 
         ~Pawn();
 };
 
